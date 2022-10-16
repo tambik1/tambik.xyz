a5e4264241ee04caafe9182689a432e9bced208f
Evgenii Tambovskii <tambik1@gmail.com>
2022-10-16 15:43:01 +0200

    Setting auto deployment configuration file

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,27 +4,27 @@ set -e
 echo "Deployment started ..."
 # Enter maintenance mode or return true
 # if already is in maintenance mode(!)
-(/opt/php80/bin/php artisan down) || true
+(php artisan down) || true
 
 # Pull the latest version of the app
 git pull origin master
 
 # Install composer dependencies(!)
-#composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
 # Clear the old cache(!)
-/opt/php80/bin/php artisan clear-compiled
+php artisan clear-compiled
 
 # Recreate cache(!)
-/opt/php80/bin/php artisan optimize
+php artisan optimize
 
 # Compile npm assets(!)
 npm run build
 
 # Run database migrations(!)
-/opt/php80/bin/php artisan migrate --force
+php artisan migrate --force
 
 # Exit maintenance mode(!)
-/opt/php80/bin/php artisan up
+php artisan up
 
 echo "Deployment finished!"
